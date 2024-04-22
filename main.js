c34d10cb3cf24338c7128fe50f3fb8c631d7bb5a
Mouli <mouli@gmail.com>
2024-04-22 20:07:02 +0530

    urlencoded add

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(express.urlencoded());
 app.get('/', (req, res)=> {
     res.send("tu bhadwa");
 })
