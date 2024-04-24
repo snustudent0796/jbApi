49481781bcad0fa30a99b2a471d821412718141a
manjeetsharma0796 <manjeetsharma0796@gmail.com>
2024-04-25 00:09:22 +0530

    add: /json

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.urlencoded());
-app.get('/', (req, res)=> {
-    res.send("tu bhadwa");
-})
+app.get("/", (req, res) => {
+  res.send("tu bhadwa");
+});
+
+app.get("/json", (req, res) => {
+  const lDist = Math.random() * 100;
+  const rDist = Math.random() * 100;
+  const suitData = { lDist, rDist };
+  res.send(JSON.stringify(suitData));
+});
+
 const port = process.env.port || 8000;
-app.listen(port);
+app.listen(port, console.log(`LISTENING ON ${port} http://localhost:${port}`));
